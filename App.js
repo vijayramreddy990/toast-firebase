30a291b491a0ee46d61149926744bbc8f2c69863
vijay <vijayramreddy990@gmail.com>
2022-08-08 13:03:30 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text, View, StyleSheet, ToastAndroid} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
